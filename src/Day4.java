90c3d6404c8b4aba7a57b9efe09c41a92776105f
Abdullah <abodi.imz@gmail.com>
2023-01-12 19:23:02 +0300

    Project1 added

---
+++ src/Day4.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.regex.*;
+public class Day4 {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile(".s");//. represents single character
+        String pass = new Scanner(System.in).nextLine();
+
+
+        char [] chars = pass.toCharArray();
+        System.out.println(has2Digits(chars));
+
+//3rd way
+
+
+    }
+
+    public static boolean has2Digits(char [] arr){
+        if(arr.length<10){
+            return false;
+        }
+        int counter=0;
+        int counter1=0;
+                // 65 - 90          97 - 122
+        for (char c: arr){
+            if(c<58 && c>47){
+                counter++;
+            }else {
+                if ((int)c>64 && (int)c<91 || (int)c>96 && (int)c<123){
+                    counter1++;
+                }else {
+                    return false;
+                }
+            }
+        }
+        if(counter>1 && counter1>1){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/Project1.java
@@ -0,0 +1,166 @@
+import java.util.Scanner;
+
+public class Project1 {
+
+    static String selected="";
+    static char [][] ticTac;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        initTicTac();
+        System.out.println("Welcome to Tic Tac Toe");
+        printTicTac();
+
+        System.out.println();
+        while (true) {
+            System.out.print("Enter your choice: ");
+            try {
+                int choice = Integer.parseInt(sc.next());
+                if (choice>9 || choice<1) {
+                    System.out.println("Invalid number!");
+                    continue;
+                }
+                if(selectPlace(choice,'X')==-1){
+                    continue;
+                };
+
+                printTicTac();
+                if(checkWinner()){
+                    System.out.println("Press enter to continue..");
+                    sc.nextLine();
+                    sc.nextLine();
+                    continue;
+                }
+                computerTurn();
+                printTicTac();
+                if(checkWinner()){
+                    System.out.println("Press enter to continue..");
+                    sc.nextLine();
+                    sc.nextLine();
+                }
+
+            }catch (NumberFormatException e){
+                System.out.println("Invalid input enter integer!");
+            }
+
+        }
+    }
+
+    public static int selectPlace(int choice, char fill){
+        int j = (choice%3);
+        if(choice%3==0&&choice<4){
+            j=choice-1;
+        }else if(choice%3==0&&choice>4){
+            j=2;
+        }
+        else {
+            j=(choice%3)-1;
+        }
+        int i = 0;
+        if(choice>3&& choice<7){
+            i=1;
+        }else if(choice>6 && choice<10){
+            i=2;
+        }
+        if(selected.contains(choice+"")){
+            if(fill=='X') {
+                System.out.println("Already taken place!");
+            }
+            return -1;
+        }else {
+            selected+=choice;
+        }
+        ticTac[i][j] = fill;
+        if(fill=='O'){
+            System.out.println("Coputer choice: "+choice);
+        }
+        return 1;
+    }
+
+    public static void initTicTac(){
+        ticTac = new char[3][3];
+        for (int i=0; i<ticTac.length;i++){
+            for (int j=0;j<ticTac[i].length; j++){
+                ticTac[i][j]=' ';
+            }
+        }
+    }
+
+    public static void printTicTac(){
+        for (int i=0; i<ticTac.length;i++){
+            System.out.print("\t\t");
+            for (int j=0; j<ticTac[i].length; j++){
+                System.out.print(ticTac[i][j]);
+                if(ticTac[i].length-1 != j){
+                    System.out.print(" | ");
+                }
+            }
+            if(i!=ticTac.length-1) {
+                System.out.println("\n\t\t---------");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void computerTurn(){
+        while (true) {
+            int place = (int) (Math.random() * 9)+1;
+            int result = selectPlace(place,'O');
+            if(result==1){
+                break;
+            }
+        }
+    }
+
+    public static boolean checkWinner(){
+        // patterns
+        // 123
+        // 456
+        // 789
+        // 159
+        // 753
+        // 147
+        // 258
+        // 369
+
+        boolean flag=true;
+        if(ticTac[0][0] == ticTac[0][1] && ticTac[0][0] == ticTac[0][2] && !(ticTac[0][0]+""+ticTac[0][1]+ticTac[0][2]+"").contains(" ")){
+            weHaveAWinner(ticTac[0][0]);
+        } else if (ticTac[1][0] == ticTac[1][1] && ticTac[1][0] == ticTac[1][2] && !(ticTac[1][0]+""+ticTac[1][1]+ticTac[1][2]).contains(" ")) {
+            weHaveAWinner(ticTac[1][0]);
+        } else if (ticTac[2][0] == ticTac[2][1] && ticTac[2][0] == ticTac[2][2] && !(ticTac[2][0]+""+ticTac[1][1]+ticTac[2][2]+"").contains(" ")) {
+            weHaveAWinner(ticTac[2][0]);
+        } else if (ticTac[0][0] == ticTac[1][1] && ticTac[0][0] == ticTac[2][2] && !(ticTac[0][0]+""+ticTac[1][1]+ticTac[2][2]+"").contains(" ")) {
+            weHaveAWinner(ticTac[0][0]);
+        }else if (ticTac[0][2] == ticTac[1][1] && ticTac[0][2] == ticTac[2][0] && !(ticTac[0][2]+""+ticTac[1][1]+ticTac[2][0]+"").contains(" ")) {
+            weHaveAWinner(ticTac[0][2]);
+        }else if (ticTac[0][0] == ticTac[1][0] && ticTac[0][0] == ticTac[2][0] && !(ticTac[0][0]+""+ticTac[1][0]+ticTac[2][0]+"").contains(" ")) {
+            weHaveAWinner(ticTac[0][0]);
+        }else if (ticTac[0][1] == ticTac[1][1] && ticTac[0][1] == ticTac[2][1] && !(ticTac[0][1]+""+ticTac[1][1]+ticTac[2][1]+"").contains(" ")) {
+            weHaveAWinner(ticTac[0][1]);
+        }else if (ticTac[0][2] == ticTac[1][2] && ticTac[0][2] == ticTac[2][2] && !(ticTac[0][2]+""+ticTac[1][2]+ticTac[2][2]+"").contains(" ")) {
+            weHaveAWinner(ticTac[0][2]);
+        }else {
+            flag = false;
+            boolean isFinished=true;
+            for (int i = 0; i<ticTac.length; i++){
+                for (int j=0; j<ticTac[i].length; j++){
+                    if(ticTac[i][j] == ' '){
+                        isFinished = false;
+                    }
+                }
+            }
+            if (isFinished){
+                System.out.println("We have a draw!");
+                initTicTac();
+                selected="";
+                return true;
+            }
+        }
+        return flag;
+    }
+    public static void weHaveAWinner(char winner){
+        System.out.println(winner+" has WON!");
+        selected="";
+        initTicTac();
+    }
+}
